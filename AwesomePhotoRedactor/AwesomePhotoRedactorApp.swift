58f967d075b43376d6f7bdbc7d53c76d7d343dde
Ivan <stolenhen@gmail.com>
2023-07-27 19:35:03 +0800

    Initial Commit

---
+++ AwesomePhotoRedactor/AwesomePhotoRedactorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomePhotoRedactorApp.swift
+//  AwesomePhotoRedactor
+//
+//  Created by Ivan Iashes on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct AwesomePhotoRedactorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
